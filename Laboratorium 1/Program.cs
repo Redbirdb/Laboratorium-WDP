fb45ba4be8cdcfada2838b463a5a990fcab9a858
Redbirdb <yan.yablunovskyi@microsoft.wsei.edu.pl>
2024-10-19 20:42:16 +0200

    Program.cs fixed

--- Laboratorium 1/Program.cs
+++ Laboratorium 1/Program.cs
@@ -5,6 +5,39 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello WSEI");
+            
+            Console.WriteLine("Hello World!");
+            Console.Write("##");
+            Console.Write("# #");
+            Console.Write("##");
+            Console.WriteLine();
+            Console.WriteLine("Hello World!");
+            Console.Write("##");
+            Console.Write("##");
+            Console.WriteLine("##");
+            Console.Write("##");
+            Console.Write(" ");
+            Console.Write(" ");
+            Console.WriteLine("##");
+            Console.Write("##");
+            Console.Write("##");
+            Console.WriteLine("##");
+            
+            Console.WriteLine("Cytat: \"Lorem ipsum dolor sit amet\"");
+
+            Console.WriteLine("Znak przejscia\n do następnego wiersza");
+            Console.WriteLine("Backspace\b kasuje poprzedni znak");
+            Console.WriteLine("Powrót karetki\r przesuwa kursor na początek");
+            Console.WriteLine("Form feed\f przesuwa kursor do następnego wiersza");
+            Console.WriteLine("\tWcięcie\t tabulator");
+            Console.WriteLine("Ścieżka: c:\\data\\file.txt");
+
+            Console.WriteLine("""
+                  1. byte   \1 bajt\
+                  2. int    \4 bajty\
+                  3. long   \8 bajtów\
+                  """);
+            Console.Write("\t123\n+\t235\n\t---\n\t358\n");
         }
     }
 }
